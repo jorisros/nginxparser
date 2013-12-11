37f94f1170d70d2f96a1811041518ff53f646672
Joris Ros <joris.ros@gmail.com>
2013-12-11 07:00:21 +0100

    Adding tests

--- tests/NginxparserTest.php
+++ tests/NginxparserTest.php
@@ -32,4 +32,70 @@ EOF;
 
         $this->assertEquals($expected, $actual);
     }
+
+    public function testSub() {
+        $expected = <<<EOF
+
+server {
+	listen		80;
+	server_name		localhost local serveralias;
+	access_log		/var/log/nginx/log/host.access.log;
+	
+	location / {
+		root		/usr/share/nginx/html;
+		index		index.html index.htm;
+	}
+
+}
+
+EOF;
+
+        $location = new NginxParser('location','/');
+        $location->setRoot('/usr/share/nginx/html')
+                 ->setIndex(array('index.html', 'index.htm'));
+        $server = new NginxParser('server');
+        $server->setListen(80)
+            ->setServerName(array('localhost','local','serveralias'))
+            ->setAccessLog('/var/log/nginx/log/host.access.log')
+            ->setLocation($location);
+        $actual = $server->build();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testReadfile() {
+        $expected = <<<EOF
+
+http {
+	include		/etc/nginx/mime.types;
+}
+
+EOF;
+
+        $location = new NginxParser();
+        $objs = $location->readFromFile('./tests/nginx_test_comment.conf');
+
+        //$actual = $location->build();
+        $actual = reset($objs);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testReadfileComment() {
+        $expected = <<<EOF
+
+http {
+	include		/etc/nginx/mime.types;
+}
+
+EOF;
+
+        $location = new NginxParser();
+        $objs = $location->readFromFile('./tests/nginx_test.conf');
+
+        //$actual = $location->build();
+        $actual = reset($objs);
+
+        $this->assertEquals($expected, $actual);
+    }
 }
